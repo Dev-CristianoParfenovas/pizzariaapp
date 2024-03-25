ce0490d5861a3def90d7abe7d6edf3f9c40b7987
Dev-Cristiano <devcristianoparfenovas@gmail.com>
2024-03-25 18:01:34 -0300

    atualizacao do sistema react pizzaria

--- backend/src/controllers/user/CreateUserController.ts
+++ backend/src/controllers/user/CreateUserController.ts
@@ -1,8 +1,20 @@
-import { Response, Request } from "express";
+import { Request, Response } from "express";
+import { CreateUserService } from '../../services/user/CreateUserService';
 
 class CreateUserController {
-    async handle() {
 
+    async handle(req: Request, res: Response) {
+
+        const {name, email, password} = req.body;
+
+        const createUserService = new CreateUserService();
+        const user = await createUserService.execute({
+            name,
+            email,
+            password
+        });
+
+        return res.json(user);
 
     }
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,49 @@
-import { Router, Request, Response } from 'express';
+import { Router } from "express";
+import multer from "multer";
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+
+import { CreateProductController } from "./controllers/product/CreateProductController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import uploadConfig from "./config/multer";
 
 const router = Router();
+const upload = multer(uploadConfig.upload("./tmp"));
 
-router.get('/teste', (req: Request, res: Response) => {
+//-- ROTAS USER --
+router.post("/users", new CreateUserController().handle);
 
-    return res.json({ nome: 'CP Pizza' })
+router.post("/session", new AuthUserController().handle);
 
-})
+router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+//-- ROTAS CATEGORY
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
+
+//-- ROTAS PRODUCT
+router.post(
+  "/product",
+  isAuthenticated,
+  upload.single("file"),
+  new CreateProductController().handle
+);
+
+router.get(
+  "/category/product",
+  isAuthenticated,
+  new ListByCategoryController().handle
+);
+
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 export { router };
