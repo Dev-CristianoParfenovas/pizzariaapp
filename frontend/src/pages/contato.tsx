8c62037c3ae2e7677c4d6dbc97faf7ed46cc34d8
Dev-Cristiano <devcristianoparfenovas@gmail.com>
2024-04-02 14:38:18 -0300

    Sistema completo CP Pizzaria

---
+++ frontend/src/pages/contato.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Contato() {
+  return (
+    <>
+    <Head>
+      <title>Pagina de contato</title> 
+    </Head>
+    <div>
+      <h1>Contato</h1>
+    </div>
+    </>
+  )
+}
